024d556e4e61afe7bd2c902b6d395eaddf79e8e4
Daniel Vázquez <145435572+vazqui12@users.noreply.github.com>
2024-02-21 08:52:45 +0100

    Delete Python directory

--- Python/request-1.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-response = requests.get("https://google.es")
-
-print(f"\n[+] Status code: {response.status_code}")
-print(f"\n[+] Mostrando código fuente de la respuesta:\n")
-
-with open("index.html", "w") as f:
-    f.write(response.text)
